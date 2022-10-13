4b232cdfe6fa8c4f72d3df86506b71ca4a18462a
josgilmar2 <josgilmar2@gmail.com>
2022-10-13 17:56:42 +0200

    films-list finished

--- StarsWarsGuideProject/src/app/app-routing.module.ts
+++ StarsWarsGuideProject/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmsListComponent } from './components/films/films-list/films-list.component';
 import { InitialPageComponent } from './components/initial-page/initial-page.component';
 import { PeopleListComponent } from './components/people/people-list/people-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/initial-page', pathMatch: 'full'},
   {path: 'initial-page', component: InitialPageComponent},
-  {path: 'people-list', component: PeopleListComponent}
+  {path: 'people-list', component: PeopleListComponent},
+  {path: 'films-list', component: FilmsListComponent}
 ];
 
 @NgModule({
