47d859a4a7cc4c4857f41fa090ee26592e269018
josgilmar2 <josgilmar2@gmail.com>
2022-10-13 18:09:26 +0200

    starships-list finished

--- StarsWarsGuideProject/src/app/app-routing.module.ts
+++ StarsWarsGuideProject/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FilmsListComponent } from './components/films/films-list/films-list.component';
 import { InitialPageComponent } from './components/initial-page/initial-page.component';
 import { PeopleListComponent } from './components/people/people-list/people-list.component';
+import { StarshipsListComponent } from './components/starships/starships-list/starships-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/initial-page', pathMatch: 'full'},
   {path: 'initial-page', component: InitialPageComponent},
   {path: 'people-list', component: PeopleListComponent},
-  {path: 'films-list', component: FilmsListComponent}
+  {path: 'films-list', component: FilmsListComponent},
+  {path: 'starships-list', component: StarshipsListComponent}
 ];
 
 @NgModule({
