466550791da372b917c22a86142a393aec4ba114
illoquehambre <illoquehambre@gmail.com>
2022-10-13 18:09:39 +0200

    Species-list Finish

--- StarsWarsGuideProject/src/app/app-routing.module.ts
+++ StarsWarsGuideProject/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InitialPageComponent } from './components/initial-page/initial-page.component';
 import { PeopleListComponent } from './components/people/people-list/people-list.component';
+import { SpeciesInformationComponent } from './components/species/species-information/species-information.component';
+import { SpeciesListComponent } from './components/species/species-list/species-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/initial-page', pathMatch: 'full'},
   {path: 'initial-page', component: InitialPageComponent},
-  {path: 'people-list', component: PeopleListComponent}
+  {path: 'people-list', component: PeopleListComponent},
+  {path: 'species-list', component: SpeciesListComponent},
+  { path: 'species-info/id', component: SpeciesInformationComponent}
 ];
 
 @NgModule({
--- StarsWarsGuideProject/src/app/components/species/species-information/species-information.component.ts
+++ StarsWarsGuideProject/src/app/components/species/species-information/species-information.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from 'src/app/interfaces/species.interface';
 
 @Component({
   selector: 'app-species-information',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpeciesInformationComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+
   }
+
 
 }
